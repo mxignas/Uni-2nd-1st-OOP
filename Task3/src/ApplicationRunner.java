c31b50da41b5288170220d948a356de2b9f790f5
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2020-07-10 17:12:57 +0300

    first oop assesment

---
+++ Task3/src/ApplicationRunner.java
@@ -0,0 +1,12 @@
+
+import java.io.File;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+
+        String dataFile = System.getProperty("user.dir") + File.separator + "lexicon.txt";
+
+    }
+    
+}
